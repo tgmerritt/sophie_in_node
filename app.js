fe2e9795eb9969d3a62f80bdaabba94a207f72d4
Tyler Merritt <tgmerritt@gmail.com>
2019-09-05 14:10:37 -0500

    CSS tweaks

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view options', {
+    layout: false
+});
 
 app.use(logger('dev'));
 // app.use(express.json());
